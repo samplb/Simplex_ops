6bcbd5057de21b6704b0395caf17927eb6319a40
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-09 19:33:11 +0100

    function test

---
+++ Simplex_ops/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Programm to Calculate Simplex Algorithms
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Bernhard Sampl
+ *
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
