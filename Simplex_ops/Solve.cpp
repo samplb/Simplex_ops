2854d0c5e6cd3e733b06e3c68ef4e6c60aedcaa8
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-13 00:36:53 +0100

    solve

--- Simplex_ops/Solve.cpp
+++ Simplex_ops/Solve.cpp
@@ -5,12 +5,46 @@
  */
 
 #include"Solve.h"
+#include<iostream>
+#include<Eigen/Core>
+//using namespace Eigen;
+using namespace std;
+Solve::Solve(){
+    cout<<"Start solvefunc"<<endl;
 
-double* Solve::lpsolve(){
-    double x=0;
-//    return &x;
 };
+Solve::~Solve(){
+    cout<<"end of Solve function"<<endl;
+};
+
 double* Solve::lpsolve(int n, double* c, int k, double** A, double* b){
-    double solve=0;
-//    return &solve;
+    // c hat länge n+k wegen schlupfvariablen, 
+    //  b hat länge kk; wegen spaltenbeschriftung.
+    // A hat A[zeilen][spalten];spalten ist n+k+1 wegen schlupfvariablen und der 
+    int col=n+k+1;
+    Eigen::MatrixXd tableau(k+1,col);//ausgangstabelle
+    tableau.setZero();
+    Eigen::VectorXd vc(n+k); //Vectpr c ist die zielfunktion
+    Eigen::VectorXd vb(k);  //vector b sind die werte
+    double z=0;    //zielwert, somit ergebnis
+    cout<<"vectorb: "<<endl;
+    for(int i=0;i<k;i++){
+        vb(i)=b[i];
+        cout<<" "<<vb(i);
+    }
+    cout<<endl<<"vectorc: "<<endl;
+    for(int j=0;j<(n+k);j++){
+        vc(j)=c[j];
+        cout<<" "<<vc(j);
+    }
+    cout<<endl<<"Ausgangstableau: "<<endl;
+    for(int l=0;l<k+1;l++){
+        for(int m=0;m<col;m++){
+//            tableau.;
+            cout<<" "<<tableau(l,k);
+                   
+        }
+        cout<<endl;
+    }
+    return NULL;
 };
