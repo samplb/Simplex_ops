73a6e20361c8ff76bc11b9c90b2d3e3c6dd1165c
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-12 11:10:49 +0100

    werte in matrizen und pfadangabe.

--- Simplex_ops/main.cpp
+++ Simplex_ops/main.cpp
@@ -30,10 +30,17 @@ int main(int argc, char** argv) {
     int k=0;
     ifstream file;
     filebuf *x=file.rdbuf();
-    x->open("ops.txt",ios_base::in);
-//   file is open.
+    string path="C:/Users/Bernhard Sampl/Documents/NetBeansProjects/a1401504-unet.univie.ac.at/Simplex_ops/Textfile/ops.txt";
+    
+    //-------------------------------------------------------------------------------------------------------------------
+    
+    
+//    /*
+    
+//   file is open. 
     try{
-    if(!file){
+       x->open(path,ios_base::in);
+    if(!file.is_open()){
         cerr << "Einlesefehler"<<file.exceptions()<<endl;
         return 2;
     }
@@ -51,19 +58,21 @@ int main(int argc, char** argv) {
     werteuebergabe.append(inputtemp);
 //    cout<<werteuebergabe<<endl;
 //    converts the string in an vector<double>
+//    */
+    
     Convertdouble cs;
     temp=cs.convertstring(werteuebergabe);
 /* output of double-values in vector.
     cout << "vector contains:";
   for (unsigned i=0; i<temp.size(); ++i)
     cout << ' ' << temp[i]<<endl;
-*/
+ * */
     n=temp[0];
     k=temp[1];
     z=temp.size();  //amount of elements.
     int zz=(k*(n+1))+2+n;
 //    creates Matrix and Vectors with values.
-    if(z-1!=zz){
+    if(z!=zz){
         cerr<<"Fehler bei der Matrixgröße!"<<"_z_"<<z<<"_zz_"<<zz<<endl;
         return 2;
     }
@@ -72,25 +81,41 @@ int main(int argc, char** argv) {
     double b[k];
     zaelwert+=2;//beginn 2te zeile
     double **A=nullptr;
+    //array erzeugen mit n spatlten und k zeilen.
     A=new double* [n];
     for(int i=0; i<n;i++){
         A[i]=new double[k];
     }
-    for(int vc=0;vc<n;vc<n){
+//    cout<<"c: _ ";
+    for(int vc=0;vc<n;vc++){
         c[vc]=temp[vc+2];
+//        cout<<c[vc]<<"  ";
     }
     zaelwert+=n;//wert, der sofort in der 3ten zeile beginnt.
-        int temo=zaelwert-1;
+        int temo=zaelwert;
+//    cout<<endl<<"b: _ ";
     for(int vb=0;vb<k;vb++){
         b[vb]=temp[temo+=n];
+//        cout<<" "<<b[vb];
+        temo++;
     }
-        for(int aa=0;aa<n;aa++){
-            for(int aaa=0; aaa<k;aaa++){
-                cout<<x<<endl;
+//    cout<<endl;
+        for(int aa=0;aa<k;aa++){
+//        cout<<"---------zw: ---"<<zaelwert<<" !!!! "<<temp[zaelwert]<<endl;
+            
+            for(int aaa=0; aaa<n;aaa++){
+//                cout<<"zw: "<<zaelwert<<"--WERT: "<<temp[zaelwert]<<endl;
+                A[aaa][aa]=temp[zaelwert];
+                zaelwert++;
+//                cout<<"5: "<<aaa<<endl;
+//                cout<<"Array: "<<A[aaa][aa]<<endl;
             }
+        zaelwert++;
         }
     
-    
+        cout<<"ok"<<endl;
+    for(int j=0; j<n;j++)  delete [] A[j];
+    delete [] A;
     return 0;
 }
 
