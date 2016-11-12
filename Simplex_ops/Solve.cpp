2a1f3ad9cb7e91f919147302565ce34b16a96c2a
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-12 03:41:58 +0100

    Algorithmus lpsolve

---
+++ Simplex_ops/Solve.cpp
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include"Solve.h"
+
+double* Solve::lpsolve(){
+    double x=0;
+//    return &x;
+};
+double* Solve::lpsolve(int n, double* c, int k, double** A, double* b){
+    double solve=0;
+//    return &solve;
+};
