8c770cc7b83107e7e36afe35ea65de2e23beb017
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-12 11:50:54 +0100

    convertdouble

--- Simplex_ops/Convertdouble.cpp
+++ Simplex_ops/Convertdouble.cpp
@@ -15,9 +15,10 @@ using namespace std;
 /*This function returns all numbers separetet with blank as a vector with double values.*/
 vector<double> Convertdouble::convertstring(string x) {
     int ab=x.length()+1;
+    int n=0;
+    int k=0;
     vector<double> arr;
     double u=0;
-    int anzahl=0;
     for(int i=0; i<ab;){
         string fo;
            do{
@@ -27,12 +28,18 @@ vector<double> Convertdouble::convertstring(string x) {
                    break;
                }
                fo+=x[gg];
+//               cout<<fo<<" ";
             }while(x[i]!=' ' && i<ab);
-           anzahl++;
+//            cout<<endl;
            u=atof(fo.c_str());
+//           cout<<u<<endl;
            arr.push_back(u);
            fo.empty();    
     }
+    n=arr[0];
+    k=arr[1];
+    int anzahl=(k*(n+1))+2+n;
+//    cout<<"a: "<<anzahl<<". sn_"<<ab<<endl;
     arr.resize(anzahl);
     return arr;
 }
