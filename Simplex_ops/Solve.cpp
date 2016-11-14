7815d6a5bb3f3ae1d4fe932ef3f4735aae995b13
Bernhard Sampl <a1401504@unet.univie.ac.at>
2016-11-14 14:49:53 +0100

    solve

--- Simplex_ops/Solve.cpp
+++ Simplex_ops/Solve.cpp
@@ -1,4 +1,4 @@
-/*
+/*Solve.cpp reads the parameters and returns a <double> optimal value.
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -7,6 +7,8 @@
 #include"Solve.h"
 #include<iostream>
 #include<Eigen/Core>
+#include<string>
+#include<vector>
 //using namespace Eigen;
 using namespace std;
 Solve::Solve(){
@@ -18,15 +20,20 @@ Solve::~Solve(){
 };
 
 double* Solve::lpsolve(int n, double* c, int k, double** A, double* b){
-    // c hat länge n+k wegen schlupfvariablen, 
+//     c hat länge n+k wegen schlupfvariablen, 
     //  b hat länge kk; wegen spaltenbeschriftung.
     // A hat A[zeilen][spalten];spalten ist n+k+1 wegen schlupfvariablen und der 
-    int col=n+k+1;
-    Eigen::MatrixXd tableau(k+1,col);//ausgangstabelle
+    int col=n+k;
+    double pivot=0;
+    Eigen::MatrixXd tableau(k,col);//ausgangstabelle
+    Eigen::MatrixXd tableau1(k,col);//kopie von tableau zum rechnentabelle
+    tableau.setZero();
     tableau.setZero();
     Eigen::VectorXd vc(n+k); //Vectpr c ist die zielfunktion
     Eigen::VectorXd vb(k);  //vector b sind die werte
     double z=0;    //zielwert, somit ergebnis
+    vector<string> pv;
+    vector<string> sv;
     cout<<"vectorb: "<<endl;
     for(int i=0;i<k;i++){
         vb(i)=b[i];
@@ -37,14 +44,35 @@ double* Solve::lpsolve(int n, double* c, int k, double** A, double* b){
         vc(j)=c[j];
         cout<<" "<<vc(j);
     }
-    cout<<endl<<"Ausgangstableau: "<<endl;
-    for(int l=0;l<k+1;l++){
-        for(int m=0;m<col;m++){
-//            tableau.;
-            cout<<" "<<tableau(l,k);
+    vc*=-1;//Zielfunktion auf rechte Seite bringen für Standardform
+//    cout<<endl<<"Ausgangstableau: \n"<<tableau<<endl;
+    for(int l=0;l<k;l++){
+        tableau(l,n+l)=1;
+        for(int m=0;m<n;m++){
+            tableau(l,m)=A[m][l];
+//            cout<<" "<<A[m][l];;
                    
         }
-        cout<<endl;
+//        cout<<endl;
     }
+        cout<<endl<<tableau<<endl;
+        while(!finished(c)){
+            pivot=getPivot(tableau,*vc,*vb);
+            
+        }
     return NULL;
+};
+bool Solve::solvetablet(Eigen::MatrixXd& tableau1, Eigen::VectorXd& c, Eigen::VectorXd& b){
+    
+};
+
+double Solve::getPivot(Eigen::MatrixXd tableau1, Eigen::VectorXd& c, Eigen::VectorXd& b){
+
+};
+
+bool Solve::finished(Eigen::VectorXd f){
+    if(f.minCoeff()>=0){
+        return true;
+    }
+    return false;
 };
